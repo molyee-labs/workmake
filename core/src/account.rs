08635128b9af569a5e1e0bd8a5eae9091d0b95fb
Alex Sarapulov <1molyee@gmail.com>
2022-01-07 17:31:20 +0700

    add 'account' service impl

--- core/src/account.rs
+++ core/src/account.rs
@@ -1,9 +1,35 @@
-use domain::service::account;
+use async_trait::async_trait;
+use domain::account;
+use domain::user;
 
 pub struct Service {
+    repo: Rc<dyn account::Repo>,
+    user: Rc<dyn user::Service>,
+}
 
-} 
+pub enum Error {
+    Unknown,
+}
 
+type Result<T, E = Error> = core::result::Result<T, E>;
+
+#[async_trait]
 impl account::Service for Service {
-    
+    type Err = Error;
+
+    async fn create(&self, owner: account::Owner) -> Result<account::Id> {
+        self.repo.create(owner)
+    }
+
+    async fn get(&self, id: account::Id) -> Result<account::Account> {
+        self.repo.get(id)
+    }
+
+    async fn own(&self, owner: account::Owner) -> Result<account::Accounts> {
+        self.repo.own(owner)
+    }
+
+    async fn change_owner(&self, id: account::Id, new: account::Owner) -> Result<()> {
+        self.repo.cas_owner()
+    }
 }
